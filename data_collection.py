3eebe773cde01c8ce59c613f850d941fc9cef30b
Aviral Gupta <guptaavi352@gmail.com>
2022-11-16 15:00:20 +0530

    Removed duplicated code from data_coolection.py

--- data_collection.py
+++ data_collection.py
@@ -3,48 +3,47 @@ import nest_asyncio
 nest_asyncio.apply()
 import pandas as pd
 
-def fetch_depressive_tweets(year):
-	tags = ["#depression", "#anxiety", "#loneliness", "#hopelessness", "#depressed", '#antidepressants']
+
+def fetch_data(data_type):
+	tags = []
+	file = ""
+
+	if(data_type == "depressive"):
+		tags = ["depression", "anxiety", "loneliness", "hopelessness", "depressed", 'antidepressants']
+		file = "data/tweets_depressed.csv"
+
+	else:
+		tags = ["joy", "happy", "delight", "cheer", "bright", "anger", "fear"]
+		file = "data/tweets_non_depressed.csv"
+
+
+	start_dates = [f"2022-01-{day} 00:00:00" for day in range(10, 29)]
+	end_dates = [f"2022-01-{day} 00:00:00" for day in range(11, 30)]
+
 	for i in range(len(tags)):
 		print(tags[i])
-		c = twint.Config()
-		# c.Format = "Tweet ID: {id} | Username: {username} | Tweet: {tweet}"
-		c.Search = tags[i]
-		c.Limit = 1000
-		c.Year = year
-		c.Store_csv = True
-		c.Store_Object = True
-		c.Output = "data/tweets_depressed.csv"
-		c.Hide_output = True
-		c.Stats = True
-		c.Lowercase	= True
-		c.Filter_retweets = True
-		twint.run.Search(c)
 
-def fetch_non_depressive_tweets(year):
-	tags = ["#joy", "#happy", "#delight", "#cheer", "#bright", "#anger", "#fear"]
-	for i in range(len(tags)):
-		print(tags[i])
-		c = twint.Config()
-		# c.Format = "Tweet ID: {id} | Username: {username} | Tweet: {tweet}"
-		c.Search = tags[i]
-		c.Limit = 1000
-		c.Year = year
-		c.Store_csv = True
-		c.Store_Object = True
-		c.Output = "data/tweets_non_depressed.csv"
-		c.Hide_output = True
-		c.Stats = True
-		c.Lowercase	= True
-		c.Filter_retweets = True
-		twint.run.Search(c)
+		for j in range(len(start_dates)):
+			c = twint.Config()
+			# c.Format = "Tweet ID: {id} | Username: {username} | Tweet: {tweet}"
+			c.Limit = 1000
 
-years = [2020, 2021, 2022]
+			c.Search = tags[i]
+			c.Since = start_dates[j]
+			c.Until = end_dates[j]
+			c.Timedelta = 1
+			c.Output = file
 
-for year in years:
-	fetch_depressive_tweets(year)
-	fetch_non_depressive_tweets(year)
+			c.Store_csv = True
+			c.Store_Object = True
+			c.Hide_output = True
+			c.Stats = True
+			c.Lowercase	= True
+			c.Filter_retweets = True
+			twint.run.Search(c)
 
+fetch_data("depressive")
+fetch_data("non-depressive")
 
 depressed_tweets = pd.read_csv("data/tweets_depressed.csv")
 non_depressed_tweets = pd.read_csv("data/tweets_non_depressed.csv")
